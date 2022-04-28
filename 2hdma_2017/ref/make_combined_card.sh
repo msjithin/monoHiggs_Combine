69f23afb881578e3c36fedf0a0293ac6c07f14d1
Jithin Madhusudanan Sreekala <jithin.madhusudanan.sreekala@cern.ch>
2022-04-27 22:51:10 -0500

    cleaning up

--- 2hdma_2017/ref/make_combined_card.sh
+++
@@ -1,5 +0,0 @@
-
-
-# combine 3 channels into one
-rm datacard_monoHiggs_cmb_v2017.txt
-combineCards.py datacard_monoHiggs_etau_v2017.txt datacard_monoHiggs_mutau_v2017.txt datacard_monoHiggs_tautau_v2017.txt > datacard_monoHiggs_cmb_v2017.txt
